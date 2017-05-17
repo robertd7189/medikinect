f6fb334bdeb3c272e54ffa435e55cbdee070c1ba
Robert Dooley <robertd7189@gmail.com>
2017-05-17 12:20:32 +1000

    connecting processing

---
+++ src/main/java/com/medikinect/core1/voice/GoogleSpeechExample.java
@@ -0,0 +1,53 @@
+package com.medikinect.core1.voice;
+
+// Imports the Google Cloud client library
+import com.google.cloud.speech.spi.v1.SpeechClient;
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.protobuf.ByteString;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class GoogleSpeechExample {
+  public static void main(String... args) throws Exception {
+    // Instantiates a client
+    SpeechClient speech = SpeechClient.create();
+
+    // The path to the audio file to transcribe
+    String fileName = "././././helpfilecreated.wav";
+
+    // Reads the audio file into memory
+    Path path = Paths.get(fileName);
+    byte[] data = Files.readAllBytes(path);
+    ByteString audioBytes = ByteString.copyFrom(data);
+
+    // Builds the sync recognize request
+    RecognitionConfig config = RecognitionConfig.newBuilder()
+        .setEncoding(AudioEncoding.LINEAR16)
+        .setSampleRateHertz(16000)
+        .setLanguageCode("en-US")
+        .build();
+    RecognitionAudio audio = RecognitionAudio.newBuilder()
+        .setContent(audioBytes)
+        .build();
+
+    // Performs speech recognition on the audio file
+    RecognizeResponse response = speech.recognize(config, audio);
+    List<SpeechRecognitionResult> results = response.getResultsList();
+
+    for (SpeechRecognitionResult result: results) {
+      List<SpeechRecognitionAlternative> alternatives = result.getAlternativesList();
+      for (SpeechRecognitionAlternative alternative: alternatives) {
+        System.out.printf("Transcription: %s%n", alternative.getTranscript());
+      }
+    }
+    speech.close();
+  }
+}
--- src/main/java/com/medikinect/core1/voice/VoiceApp.java
+++ src/main/java/com/medikinect/core1/voice/VoiceApp.java
@@ -13,139 +13,173 @@ import com.google.protobuf.ByteString;
 import sun.audio.AudioPlayer;
 import sun.audio.AudioStream;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-
 @SuppressWarnings("restriction")
 public class VoiceApp {
-	
-	
-	static final long RECORD_TIME = 4000;  // 4 seconds
-  public static void main(String... args) throws Exception {
-    // Instantiates a client
-    SpeechClient speech = SpeechClient.create();
-//
-    //open the sound file as a Java input stream
-    String gongFile = "/Users/robertdooley/Downloads/Are you ok.wav";
-    InputStream in = new FileInputStream(gongFile);
 
-    // create an audiostream from the inputstream
-    AudioStream audioStream = new AudioStream(in);
+	static final long RECORD_TIME = 5000; // 4 seconds
 
-    // play the audio clip with the audioplayer class
-    AudioPlayer.player.start(audioStream);
-    
-    TimeUnit.MILLISECONDS.sleep(4000);
-    
-    
-//    figure out how to get line in and save file
-    final JavaSoundRecorder recorder = new JavaSoundRecorder();
-	 
-    // creates a new thread that waits for a specified
-    // of time before stopping
-    Thread stopper = new Thread(new Runnable() {
-        public void run() {
-            try {
-                Thread.sleep(RECORD_TIME);
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-            recorder.finish();
-        }
-    });
+	public static void main(String... args) throws Exception {
 
-    stopper.start();
+		boolean runVoice = false;
+		boolean runProgram = true;
+		boolean runFileCheck = true;
+		int counter = 0;
 
-    // start recording
-    recorder.start();
-    
-    
-    // The path to the audio file to transcribe
-    // /Users/robertdooley/Downloads
-    String fileName = ("././././helpfilecreated.wav");
-    //update
+		while (runProgram) {
+			while (runFileCheck) {
+				File file = new File("/Users/robertdooley/Documents/workspace/voice/text.txt");
+				FileReader fr = new FileReader(file);
+				char[] a = new char[10];
+				fr.read(a);
 
-    // Reads the audio file into memory
-    Path path = Paths.get(fileName);
-    byte[] data = Files.readAllBytes(path);
-    ByteString audioBytes = ByteString.copyFrom(data);
-    
-//    
-//    File format= .wav ; 16,000 Hz ; PCM compression
-//    
-//		/Users/Downloads/helphelphelp.wav
-    
-    // Builds the sync recognize request
-    RecognitionConfig config = RecognitionConfig.newBuilder()
-        .setEncoding(AudioEncoding.LINEAR16)
-        .setSampleRate(16000)
-        .build();
-    RecognitionAudio audio = RecognitionAudio.newBuilder()
-        .setContent(audioBytes)
-        .build();
+				String newRead = "";
+				for (char c : a) {
+					newRead += c;
+				}
+				fr.close();
 
-    // Performs speech recognition on the audio file
-    SyncRecognizeResponse response = speech.syncRecognize(config, audio);
-    List<SpeechRecognitionResult> results = response.getResultsList();
+				System.out.println(newRead);
+				if (newRead.contains("true")) {
+					System.out.println("The string is true, init next step.");
+					runVoice = true;
+					runFileCheck = false;
+				}
 
-    int helpCount = 0;
-    
-//    String[] helpKeywords = {
-//    	"I need help",
-//    	"help help help",
-//    	"help me",
-//    	"help needed",
-//    	"help help"
-//    };
-    
-    for (SpeechRecognitionResult result: results) {
-    	
-      List<SpeechRecognitionAlternative> alternatives = result.getAlternativesList();
-      
-      for (SpeechRecognitionAlternative alternative: alternatives) {
-//        System.out.printf("Transcription: %s%n", alternative.getTranscript());
-//       
-    	  
-    	  //"If you are in need of assistance, reply with help, help help (three times).
-    	  //"I need help"
-    	  System.out.println("T: "+alternative.getTranscript());
-    	  System.out.println("C: "+alternative.getConfidence());
-    	  
-    	  
-    	  if ((alternative.getTranscript().contains("help")) && (alternative.getConfidence() > 0.7) && !alternative.getTranscript().contains("ok")){
-    		  
-    		  System.out.print("help needed!");
-    	  }
-    	  if ((alternative.getTranscript().contains("ok")) && (alternative.getConfidence() > 0.7))
-    	  {
-    		  System.out.println("Everything is OK");
-    	  }
-    	  
-    		  
-    		  
-    	  
-    	  
-//    	  System.out.print(alternative);
-    	  
-//        if (alternative.getTranscript().equalsIgnoreCase("help"))
-//        {
-//        	helpCount++;
-//        	System.out.println(helpCount);
-//        }
-      }
-      
-    }
-    
-    if(helpCount == 3){
-    	System.out.println("Get help");
-    }
-    
-    speech.close();
-  }
+				else if (newRead.contains("false")) {
+					System.out.println("The string is false, re loop previous step, sleep for 5 seconds");
+					try {
+						Thread.sleep(RECORD_TIME);
+					} catch (InterruptedException ex) {
+						ex.printStackTrace();
+					}
+
+				}
+			}
+			while (runVoice) {
+				// Instantiates a client
+				SpeechClient speech = SpeechClient.create();
+				//
+				// open the sound file as a Java input stream
+				String gongFile = "/Users/robertdooley/Downloads/Are you ok.wav";
+				InputStream in = new FileInputStream(gongFile);
+
+				// create an audiostream from the inputstream
+				AudioStream audioStream = new AudioStream(in);
+
+				// play the audio clip with the audioplayer class
+				AudioPlayer.player.start(audioStream);
+
+				TimeUnit.MILLISECONDS.sleep(4000);
+
+				// figure out how to get line in and save file
+				final JavaSoundRecorder recorder = new JavaSoundRecorder();
+
+				// creates a new thread that waits for a specified
+				// of time before stopping
+				Thread stopper = new Thread(new Runnable() {
+					public void run() {
+						try {
+							Thread.sleep(RECORD_TIME);
+						} catch (InterruptedException ex) {
+							ex.printStackTrace();
+						}
+						recorder.finish();
+					}
+				});
+
+				stopper.start();
+
+				// start recording
+				recorder.start();
+
+				// The path to the audio file to transcribe
+				// /Users/robertdooley/Downloads
+				String fileName = ("././././helpfilecreated.wav");
+				// update
+
+				// Reads the audio file into memory
+				Path path = Paths.get(fileName);
+				byte[] data = Files.readAllBytes(path);
+				ByteString audioBytes = ByteString.copyFrom(data);
+
+				//
+				// File format= .wav ; 16,000 Hz ; PCM compression
+				//
+				// /Users/Downloads/helphelphelp.wav
+
+				// Builds the sync recognize request
+				RecognitionConfig config = RecognitionConfig.newBuilder().setEncoding(AudioEncoding.LINEAR16)
+						.setSampleRate(16000).build();
+				RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(audioBytes).build();
+
+				// Performs speech recognition on the audio file
+				SyncRecognizeResponse response = speech.syncRecognize(config, audio);
+				List<SpeechRecognitionResult> results = response.getResultsList();
+
+				int helpCount = 0;
+
+				// String[] helpKeywords = {
+				// "I need help",
+				// "help help help",
+				// "help me",
+				// "help needed",
+				// "help help"
+				// };
+
+				for (SpeechRecognitionResult result : results) {
+
+					List<SpeechRecognitionAlternative> alternatives = result.getAlternativesList();
+
+					for (SpeechRecognitionAlternative alternative : alternatives) {
+						// System.out.printf("Transcription: %s%n",
+						// alternative.getTranscript());
+						//
+
+						// "If you are in need of assistance, reply with help,
+						// help
+						// help (three times).
+						// "I need help"
+						System.out.println("T: " + alternative.getTranscript());
+						System.out.println("C: " + alternative.getConfidence());
+
+						if ((alternative.getTranscript().contains("help")) && (alternative.getConfidence() > 0.7)
+								&& !alternative.getTranscript().contains("ok")) {
+
+							System.out.print("help needed!");
+							runProgram = false;
+						}
+						if ((alternative.getTranscript().contains("ok")) && (alternative.getConfidence() > 0.7)) {
+							System.out.println("Everything is OK");
+							runProgram = true;
+						}
+
+						// System.out.print(alternative);
+
+						// if
+						// (alternative.getTranscript().equalsIgnoreCase("help"))
+						// {
+						// helpCount++;
+						// System.out.println(helpCount);
+						// }
+					}
+
+				}
+
+//				if (helpCount == 3) {
+//					System.out.println("Get help");
+//				}
+
+				speech.close();
+				runVoice = false;
+			}
+			counter++;
+		}
+	}
 }
